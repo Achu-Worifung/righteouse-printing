b9da44e708763309cc700ee6312db3c43ea9529e
Achu Worifung <achuworifung@gmail.com>
2025-12-11 01:05:10 -0700

    added route group for navigational organization

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { AuthProvider } from "@/app/context/AuthContext";
+import { Navbar } from "../components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,15 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <AuthProvider>
-          
-          {children}
-        </AuthProvider>
-      </body>
-    </html>
+    <>
+      <Navbar />
+
+      {children}
+    </>
   );
 }
