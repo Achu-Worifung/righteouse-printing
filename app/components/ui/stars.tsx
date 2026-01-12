71fc837b648fc8221229e4f18adfc79af2a0c831
worifung achu <achuclinton47@gmail.com>
2026-01-12 13:49:06 -0700

    chore:fixed stars for smaller screens

--- app/components/ui/stars.tsx
+++ app/components/ui/stars.tsx
@@ -1,33 +1,42 @@
 import { Star } from "lucide-react"
 
-export function Stars({ count, avg }: { count: number; avg: number }) {
+interface StarsProps {
+  count: number;
+  avg: number;
+  sum: number;
+}
+
+export function Stars({ count, avg,}: StarsProps) {
   return (
     <div className="flex items-center gap-1">
-      {Array.from({ length: 5 }, (_, i) => {
-        const fill = Math.max(0, Math.min(1, avg - i))
+      <div className="flex">
+        {Array.from({ length: 5 }, (_, i) => {
+          const fill = Math.max(0, Math.min(1, avg - i))
 
-        return (
-          <div key={i} className="relative md:w-5 md:h-5 w-3 h-3">
-            <Star
-              className="absolute inset-0 text-gray-300"
-              fill="currentColor"
-            />
-
-            <div
-              className="absolute inset-0 overflow-hidden"
-              style={{ width: `${fill * 100}%` }}
-            >
+          return (
+            <div key={i} className={`relative md:w-5 md:h-5 w-4 h-4`}>
               <Star
-                className="text-amber-400 md:w-5 md:h-5 w-3 h-3"
+                className="absolute inset-0 w-full h-full text-gray-300"
                 fill="currentColor"
               />
-            </div>
-          </div>
-        )
-      })}
 
-      <span className="ml-2 text-sm text-gray-600 w-full flex gap-1">
-        (<span>{count}</span> <span className="hidden md:block "> Reviews</span>)
+              <div
+                className="absolute inset-0 overflow-hidden"
+                style={{ width: `${fill * 100}%` }}
+              >
+
+                <Star
+                  className={`text-amber-400 md:w-5 md:h-5 w-4 h-4`}
+                  fill="currentColor"
+                />
+              </div>
+            </div>
+          )
+        })}
+      </div>
+
+      <span className="ml-2 text-sm text-gray-600">
+        ({count})
       </span>
     </div>
   )
