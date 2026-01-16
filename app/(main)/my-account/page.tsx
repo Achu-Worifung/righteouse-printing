10d298cbb777aed992c2d984dde153f490cce684
WORIFUNG CLINTON ACHU <achuworifung@gmail.com>
2026-01-16 03:47:29 +0000

    feat: created layout for user setting account

--- app/(main)/my-account/page.tsx
+++ app/(main)/my-account/page.tsx
@@ -1,37 +1,68 @@
 import { toast } from "sonner";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
-import {UserAccountDetails} from "@/components/ui/myaccount/user-account-details";
+import { UserAccountDetails } from "@/components/ui/myaccount/user-account-details";
+import { ChevronRight, User, CreditCard, ShieldX, BellRing } from 'lucide-react';
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 export default async function MyAccountPage() {
     const cookieStore = await cookies();
     const isAuthenticated = cookieStore.get("authToken") !== undefined;
-    if (!isAuthenticated) {
-        toast.error("You must be signed in to view your account.");
-        redirect("/signin");
-    }
+    // if (!isAuthenticated) {
+    //     toast.error("You must be signed in to view your account.");
+    //     redirect("/signin");
+    // }
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
-    const res = await fetch(`${baseUrl}/api/auth/me/user`, { 
+    const res = await fetch(`${baseUrl}/api/auth/me/user`, {
         method: "GET",
         headers: {
             'Cookie': `authToken=${cookieStore.get("authToken")?.value}`
         }
     });
-    const data = await res.json();
-    if (!data.authenticated) {
-        toast.error("You must be signed in to view your account.");
-        redirect("/signin");
-    }else 
-    {
-        console.log("User Data:", data.user);
-    }
+    // const data = await res.json();
+    // if (!data.authenticated) {
+    //     toast.error("You must be signed in to view your account.");
+    //     redirect("/signin");
+    // }else 
+    // {
+    //     console.log("User Data:", data.user);
+    // }
 
-  return (
-    <div className="p-4">
-      <UserAccountDetails user={data.user} />
-      {/* <Addresses addresses={data.user.addresses || []} />
+    return (
+        <div className="p-4">
+            <h1 className="text-2xl sm:text-4xl text-semibold sm:text-bold">Settings</h1>
+            <p>Manage your account settings and preferences.</p>
+            <Tabs
+                defaultValue="personal-info"
+                className=" mt-5 w-full"
+            >
+                <TabsList className="  mx-auto [&>button]:px-6 [&>button]:tracking-wide [&>button]:text-semibold ">
+                    <TabsTrigger value="personal-info" className="px-4">
+                        <User />Personal Information
+                    </TabsTrigger>
+                    <TabsTrigger value="email&password" className="px-4">
+                        <ShieldX /> Email & Password
+                    </TabsTrigger>
+                    <TabsTrigger value="payment" className="px-4">
+                        <CreditCard />Payment
+                    </TabsTrigger>
+                    <TabsTrigger value="notification" className="px-4">
+                        <BellRing /> Notifications
+                    </TabsTrigger>
+                </TabsList>
+
+                <TabsContent value="personal-info">
+                    <UserAccountDetails/>
+                </TabsContent>
+                <TabsContent value="email&password">
+                    Change your password here.
+                </TabsContent>
+            </Tabs>
+            {/* <Addresses addresses={data.user.addresses || []} />
       <Payment userId = {data.user._id} /> */}
 
-      
-    </div>
-  );
+
+        </div>
+    );
 }
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -23,43 +23,44 @@ import { Separator } from "@radix-ui/react-dropdown-menu";
 
 export function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const { isAuthenticated, logout} = useAuth();
+  const { isAuthenticated, logout } = useAuth();
 
   return (
     <header className="w-full relative">
       <nav className="font-light text-xl drop-shadow-2xl w-full flex justify-between items-center py-2 bg-white/10 backdrop-blur-3xl px-2 md:px-8 lg:px-16">
         <div className="flex flex-col gap-1 md:w-auto w-full">
           <div className="flex  justify-between items-center flex-row px-4 md:w-auto w-full">
-            <Logo className="relative left-0 p-0"/>
+            <Logo className="relative left-0 p-0" />
             <div className="md:hidden flex items-center gap-4">
-                <Handbag size={32} />
-                <DropdownMenu>
-                  <DropdownMenuTrigger>
-                    {" "}
-                    <UserRound size={34} />
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent className="font-light text-xl">
-                    {isAuthenticated ? (
-                      <>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem className="font-light text-xl">My Account</DropdownMenuItem>
-                        <DropdownMenuItem className="font-light text-xl">Orders</DropdownMenuItem>
-                        <DropdownMenuItem onClick={logout} className="font-light text-xl">
-                          Sign Out
-                        </DropdownMenuItem>
-                      </>
-                    ) : (
-                      <>
-                        <DropdownMenuItem className="font-light text-xl">
-                          <Link href="/signin">Sign In</Link>
-                          </DropdownMenuItem>
-                        <DropdownMenuItem className="font-light text-xl">
-                          <Link href="/signup" className="text-xl font-light">Sign Up</Link>
-                        </DropdownMenuItem>
-                      </>
-                    )}
-                  </DropdownMenuContent>
-                </DropdownMenu>
+              <Link href={"/cart"}> <Handbag size={32} /></Link>
+
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  {" "}
+                  <UserRound size={34} />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="font-light text-xl">
+                  {isAuthenticated ? (
+                    <>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem className="font-light text-xl">My Account</DropdownMenuItem>
+                      <DropdownMenuItem className="font-light text-xl">Orders</DropdownMenuItem>
+                      <DropdownMenuItem onClick={logout} className="font-light text-xl">
+                        Sign Out
+                      </DropdownMenuItem>
+                    </>
+                  ) : (
+                    <>
+                      <DropdownMenuItem className="font-light text-xl">
+                        <Link href="/signin">Sign In</Link>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem className="font-light text-xl">
+                        <Link href="/signup" className="text-xl font-light">Sign Up</Link>
+                      </DropdownMenuItem>
+                    </>
+                  )}
+                </DropdownMenuContent>
+              </DropdownMenu>
               {isMenuOpen ? (
                 <X
                   size={32}
@@ -75,32 +76,32 @@ export function Navbar() {
               )}
             </div>
 
+          </div>
+          {isMenuOpen && (
+            <div className="w-full flex flex-col text-lg font-light backdrop-blur-3xl text-start items-start  rounded-lg gap-4 md:hidden px-4 py-4 -webkit-text-stroke:1px_black text-black  ">
+              <Link href="/" onClick={() => setIsMenuOpen(false)} className="">
+                Home
+                <Separator />
+              </Link>
+              <Accordion type="single" collapsible className="w-full">
+                <AccordionItem value="item-1">
+                  <AccordionTrigger className="w-full text-xl font-light">
+                    Shop
+                  </AccordionTrigger>
+                  <AccordionContent className="flex flex-col gap-2 mt-2 ml-4 text-xl font-light">
+                    <Link href="/listing">All</Link>
+                    <Link href="/listing?category=tshirts">T-Shirts</Link>
+                    <Link href="/listing?category=hoodies">Hoodies</Link>
+                    <Link href="/listing?category=caps">Caps</Link>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+              <Link href="" onClick={() => setIsMenuOpen(false)} className="">
+                New Arrivals
+                <Separator />
+              </Link>
             </div>
-            {isMenuOpen && (
-              <div className="w-full flex flex-col text-lg font-light backdrop-blur-3xl text-start items-start  rounded-lg gap-4 md:hidden px-4 py-4 -webkit-text-stroke:1px_black text-black  ">
-                <Link href="" onClick={() => setIsMenuOpen(false)} className="">
-                  Home
-                  <Separator />
-                </Link>
-                <Accordion type="single" collapsible className="w-full">
-                  <AccordionItem value="item-1">
-                    <AccordionTrigger className="w-full text-xl font-light">
-                      Shop
-                    </AccordionTrigger>
-                    <AccordionContent className="flex flex-col gap-2 mt-2 ml-4 text-xl font-light">
-                      <Link href="/listing">All</Link>
-                      <Link href="/listing?category=tshirts">T-Shirts</Link>
-                      <Link href="/listing?category=hoodies">Hoodies</Link>
-                      <Link href="/listing?category=caps">Caps</Link>
-                    </AccordionContent>
-                  </AccordionItem>
-                </Accordion>
-                <Link href="" onClick={() => setIsMenuOpen(false)} className="">
-                  New Arrivals
-                  <Separator />
-                </Link>
-              </div>
-            )}
+          )}
         </div>
         <div className="hidden md:flex gap-4 md:gap-12 stroke-black [&_span]:[-webkit-text-stroke:1px_black]">
           <Link href="/" className="">Home</Link>
@@ -121,7 +122,7 @@ export function Navbar() {
           <Link href="/new-arrivals" className="text-2xl font-light">New Arrivals</Link>
         </div>
         <div className="items-center gap-6 cursor-pointer hidden md:flex justify-center">
-          <Handbag size={32} />
+          <Link href={"/cart"}> <Handbag size={32} /></Link>
           <DropdownMenu>
             <DropdownMenuTrigger>
               {" "}
@@ -136,14 +137,14 @@ export function Navbar() {
                   <DropdownMenuItem onClick={logout} className="font-light text-xl">Sign Out</DropdownMenuItem>
                 </>
               ) : (
-                 <>
-                        <DropdownMenuItem className="font-light text-xl">
-                          <Link href="/signin">Sign In</Link>
-                          </DropdownMenuItem>
-                        <DropdownMenuItem className="font-light text-xl">
-                          <Link href="/signup" className="text-xl font-light">Sign Up</Link>
-                        </DropdownMenuItem>
-                      </>
+                <>
+                  <DropdownMenuItem className="font-light text-xl">
+                    <Link href="/signin">Sign In</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="font-light text-xl">
+                    <Link href="/signup" className="text-xl font-light">Sign Up</Link>
+                  </DropdownMenuItem>
+                </>
               )}
             </DropdownMenuContent>
           </DropdownMenu>
